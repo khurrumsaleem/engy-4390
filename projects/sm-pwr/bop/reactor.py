78ce42d64d7ee81f664040fb9bd5cc0f72b7e5eb
NikolausKemper <72827486+NikolausKemper@users.noreply.github.com>
2020-12-10 17:24:10 -0500

    Update reactor.py

--- projects/sm-pwr/bop/reactor.py
+++ projects/sm-pwr/bop/reactor.py
@@ -558,8 +558,12 @@ class SMPWR(Module):
 
         rho_t = self.rho_0 + alpha_n * (n_dens - n_dens_ref) + \
                              alpha_tn * (temp - temp_ref)
+<<<<<<< HEAD
         if time > 2400:
             rho_t -= 1e-3
+=======
+
+>>>>>>> parent of 93103be... Reactor shutdown
         return rho_t
 
     def __q_source(self, time):
